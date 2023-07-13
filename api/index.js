ed34504c6026cbc2306d8ef93f79bfd09bedfba1
SamGerrick <sgcdesilva@unionbankph.com>
2023-07-13 21:16:09 +0800

    Test

--- api/index.js
+++ api/index.js
@@ -1,45 +1,51 @@
 import axios from 'axios'
-//LocalHost
-// const baseUrl = 'http://localhost:5000'
 //Live Server
 const baseUrl = 'https://next-todo-list-v1-server.onrender.com'
 
 export const getAllTodos = async () => {
-    const response = await axios.get(`${baseUrl}/todos`)
-    const todos = await response.data
+    const res = await fetch(`${baseUrl}/todos`, { cache: 'no-store' })
+    const todos = await res.json()
     return todos
 }
 
 export const addTodo = async (todo) => {
-    const res = await axios.post(`${baseUrl}/todos`, todo, {
+    const res = await fetch(`${baseUrl}/todos`, {
+        method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
+        body: JSON.stringify(todo),
     })
-    const newTodo = await res.data
+    const newTodo = await res.json()
     return newTodo
 }
 
 export const updateTodo = async (todo) => {
-    const res = await axios.put(`${baseUrl}/todos/${todo.id}`, todo, {
+    const res = await fetch(`${baseUrl}/todos/${todo.id}`, {
+        method: 'PUT',
         headers: {
             'Content-Type': 'application/json',
         },
+        body: JSON.stringify(todo),
     })
-    const updatedTodo = await res.data
+    const updatedTodo = await res.json()
     return updatedTodo
 }
 
 export const updateStatus = async (todo) => {
-    const res = await axios.put(`${baseUrl}/todos/${todo.id}`, todo, {
+    const res = await fetch(`${baseUrl}/todos/${todo.id}`, {
+        method: 'PUT',
         headers: {
             'Content-Type': 'application/json',
         },
+        body: JSON.stringify(todo),
     })
-    const updatedTodoStatus = await res.data
+    const updatedTodoStatus = await res.json()
     return updatedTodoStatus
 }
 
 export const deleteTodo = async (id) => {
-    await axios.delete(`${baseUrl}/todos/` + id)
+    await fetch(`${baseUrl}/todos/` + id, {
+        method: 'DELETE',
+    })
 }
